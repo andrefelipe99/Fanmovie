d2644f772d5e803c4af36eb836fbac42a3424fa5
Gabriel Gomes <gabrielgomesdesantana@gmail.com>
2022-05-04 17:06:30 -0300

    Projeto em spring boot criado

---
+++ src/main/java/com/fanmovie/fanmovie/FanmovieApplication.java
@@ -0,0 +1,13 @@
+package com.fanmovie.fanmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanmovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fanmovie/fanmovie/FanmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanmovie.fanmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
